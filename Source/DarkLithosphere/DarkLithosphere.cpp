6731fed485b627dae05b9b2c7ec9b419ec7548b6
dd2012 <demondark20@gmail.com>
2022-08-17 23:06:16 +0300

    fix: save dir error

--- Source/DarkLithosphere/DarkLithosphere.cpp
+++ Source/DarkLithosphere/DarkLithosphere.cpp
@@ -3,5 +3,26 @@
 #include "DarkLithosphere.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DarkLithosphere, "DarkLithosphere" );
+IMPLEMENT_PRIMARY_GAME_MODULE( FDarkLithosphereGameModule, DarkLithosphere, "DarkLithosphere" );
  
+
+
+void FDarkLithosphereGameModule::StartupModule() {
+	UE_LOG(LogTemp, Warning, TEXT("FDarkLithosphereGameModule::StartupModule()"));
+
+	FString SaveDir = FPaths::ProjectSavedDir() + TEXT("Map/");
+	UE_LOG(LogTemp, Log, TEXT("Check directory: %s"), *SaveDir);
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+	if (!PlatformFile.DirectoryExists(*SaveDir)) {
+		PlatformFile.CreateDirectory(*SaveDir);
+		UE_LOG(LogTemp, Log, TEXT("Create directory: %s"), *SaveDir);
+		if (!PlatformFile.DirectoryExists(*SaveDir)) {
+			UE_LOG(LogTemp, Warning, TEXT("Unable to create directory: %s"), *SaveDir);
+		}
+	}
+}
+
+
+void FDarkLithosphereGameModule::ShutdownModule() {
+
+}
--- Source/DarkLithosphere/DarkLithosphere.h
+++ Source/DarkLithosphere/DarkLithosphere.h
@@ -3,3 +3,16 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+
+class FDarkLithosphereGameModule : public IModuleInterface
+{
+public:
+    virtual void StartupModule() override;
+    virtual void ShutdownModule() override;
+
+    virtual bool IsGameModule() const override {
+        return true;
+    }
+
+};
