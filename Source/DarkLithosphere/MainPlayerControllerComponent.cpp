5f5ef3b7916ea582b4af2f4ea7f2541f7acb72da
dd2012 <demondark20@gmail.com>
2023-08-13 12:58:00 +0300

    fix: inventory

--- Source/DarkLithosphere/MainPlayerControllerComponent.cpp
+++ Source/DarkLithosphere/MainPlayerControllerComponent.cpp
@@ -613,15 +613,12 @@ bool UMainPlayerControllerComponent::ToggleCraftMode(int ReceiptId) {
 		if (CraftRecipeData->bToInventory) {
 			UContainerComponent* Inventory = MainController->GetInventory();
 			if (Inventory != nullptr) {
-				TSubclassOf<ASandboxObject> ObjSubclass = MainController->LevelController->GetSandboxObjectByClassId(CraftRecipeData->SandboxClassId);
-				if (ObjSubclass) {
-					auto* Obj = Cast<ASandboxObject>(ObjSubclass->GetDefaultObject());
-					for (int Idx = 0; Idx < 10; Idx++) {
-						Inventory->AddObject(Obj);
-					}
-
-					return false;
+				for (int Idx = 0; Idx < 10; Idx++) {
+					//Inventory->AddObject(Obj);
+					MainController->SandboxAddItem(CraftRecipeData->SandboxClassId);
 				}
+
+				return false;
 			}
 		} else {
 			ResetState();
